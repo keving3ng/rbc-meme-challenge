ae23c99be7169c37ae847316fbf09ebcc2b535a9
Kevin Geng <kevin.geng@quarksys.ca>
2020-01-17 14:51:59 -0500

    created function structure

--- meme-filter.py
+++ meme-filter.py
@@ -2,6 +2,8 @@
 import requests
 
 API = "https://memeservice.cfapps.io/api/memes"
+PTS_THRESHOLD = 60000
+
 
 try:
     response = requests.get(API)
@@ -9,4 +11,12 @@ try:
 except:
     print("Error")
 
-print(data)
+def filter_memes(data, threshold=60000):
+    return 0
+
+def sum_total_points(data):
+    total_pts = sum([meme['points'] for meme in data)
+    return total_pts
+
+def calc_rank(rank):
+    return 0
